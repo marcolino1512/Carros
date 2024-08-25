444181d724852541088515046c19db2216d3e9b8
Marco Antonio Nascimento <137643410+marcolino1512@users.noreply.github.com>
2024-08-25 14:19:38 -0300

    Carros

---
+++ main.js
@@ -0,0 +1,81 @@
+let menu=document.querySelector("#menu-btn");
+let navbar=document.querySelector('.navbar');
+
+
+menu.onclick=()=>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+document.querySelector("#login-btn").onclick=()=>{
+    document.querySelector(".login-form-container").classList.toggle("active");
+}
+
+document.querySelector("#close-login-form").onclick=()=>{
+    document.querySelector(".login-form-container").classList.remove("active");
+}
+window.onload=()=>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
+
+document.querySelector(".home").onmousemove= (e) =>{
+    document.querySelectorAll(".home-parallax").forEach (elm=>{
+
+        let speed =elm.getAttribute('data-speed');
+
+        let x=(window.innerWidth - e.pageX * speed) / 90;
+        let y=(window.innerHeight - e.pageY * speed) / 90;
+
+        elm.style.transform=`translateX(${y}px) translateY(${x}px) `
+
+    });
+};
+
+document.querySelector(".home").onmouseleave= () =>{
+    document.querySelectorAll(".home-parallax").forEach (elm=>{
+
+        elm.style.transform=`translateX(0px) translateY(0px)`;
+
+    });
+};
+
+var swiper = new Swiper(".featured-slider", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    loop: true,
+      autoplay:{
+        delay:2500,
+        disableOnInteraction: false,
+      }
+    },
+  });
+  var swiper = new Swiper(".vehicles-slider", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    loop: true,
+      autoplay:{
+        delay:2500,
+        disableOnInteraction: false,
+      }
+    },
+  });
+  var swiper = new Swiper(".reviews-slider", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    loop: true,
+      autoplay:{
+        delay:2500,
+        disableOnInteraction: false,
+      }
+    },
+  });
+
+  
+  
